1fae508365bde5a97c6582ed13bff908d656fc15
李盼庚 <lipg@outlook.com>
2020-07-13 18:50:36 +0800

    Testing

--- pods.go
+++ pods.go
@@ -2,10 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	v1 "k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/klog"
 	"net/http"
@@ -112,83 +110,83 @@ func generatePatch(ar v1.AdmissionReview, pod corev1.Pod) []Patch {
 	}
 	patches = append(patches, Patch{OP: OP_ADD, Path: "/metadata/labels/skywalking-enabled", Value: "enabled"})
 	patches = append(patches, Patch{OP: OP_ADD, Path: "/metadata/labels/skywalking-timestamp", Value: time.Now()})
-
-	// addVolume
-	swVolumeQuantity, _ := resource.ParseQuantity("200Mi")
-	swVolume := corev1.Volume{
-		Name: "skywalking",
-		VolumeSource: corev1.VolumeSource{
-			EmptyDir: &corev1.EmptyDirVolumeSource{
-				Medium:    corev1.StorageMediumDefault,
-				SizeLimit: &swVolumeQuantity,
-			},
-		},
-	}
-	if len(pod.Spec.Volumes) == 0 {
-		patches = append(patches, Patch{OP: OP_ADD, Path: "/spec/volumes", Value: [0]struct{}{}})
-	}
-	patches = append(patches, Patch{OP: OP_ADD, Path: "/spec/volumes/", Value: swVolume})
-
-	// addInitContainer
-	initContainer := corev1.Container{
-		Name:  "skywalking",
-		Image: config.SWImage,
-		VolumeMounts: []corev1.VolumeMount{
-			{
-				Name:      "skywalking",
-				MountPath: "/opt/skywalking",
-			},
-		},
-	}
-	if len(pod.Spec.InitContainers) == 0 {
-		patches = append(patches, Patch{OP: OP_ADD, Path: "/spec/initContainers", Value: [0]struct{}{}})
-	}
-	patches = append(patches, Patch{OP: OP_ADD, Path: "/spec/initContainers/", Value: initContainer})
-
-	// container cycle
-	for ic, container := range pod.Spec.Containers {
-		if containerMatching(container) {
-			mountPath := fmt.Sprintf("/spec/containers/%d/volumeMounts", ic)
-
-			// volumeMount
-			mount := corev1.VolumeMount{
-				Name:      "skywalking",
-				MountPath: "/opt/skywalking",
-			}
-			if len(container.VolumeMounts) == 0 {
-				patches = append(patches, Patch{OP: OP_ADD, Path: mountPath, Value: [0]struct{}{}})
-			}
-			patches = append(patches, Patch{OP: OP_ADD, Path: mountPath + "/", Value: mount})
-
-			// add Init Command and Pods Env
-			envPath := fmt.Sprintf("/spec/containers/%d/env", ic)
-			envSWArg := "-javaagent:/opt/skywalking/skywalking-agent.jar"
-			envOP := OP_ADD
-			if len(container.Env) != 0 {
-				for ie, env := range container.Env {
-					if env.Name == "JAVA_TOOL_OPTIONS" {
-						if len(env.Value) != 0 {
-							envSWArg = env.Value + " " + envSWArg
-							envPath = envPath + "/" + string(ie)
-							envOP = OP_REPLACE
-						}
-					}
-				}
-			}
-			if len(container.Env) == 0 {
-				patches = append(patches, Patch{OP: OP_ADD, Path: envPath, Value: [0]struct{}{}})
-			}
-			if envOP == OP_REPLACE {
-				patches = append(patches, Patch{OP: envOP, Path: envPath, Value: envSWArg})
-			} else {
-				patches = append(patches, Patch{OP: envOP, Path: envPath + "/",
-					Value: corev1.EnvVar{Name: "JAVA_TOOL_OPTIONS", Value: envSWArg}})
-			}
-		}
-	}
-	if klog.V(2) {
-		res, _ := json.Marshal(patches)
-		klog.V(2).Info("Patchs: ", string(res))
-	}
+	//
+	//// addVolume
+	//swVolumeQuantity, _ := resource.ParseQuantity("200Mi")
+	//swVolume := corev1.Volume{
+	//	Name: "skywalking",
+	//	VolumeSource: corev1.VolumeSource{
+	//		EmptyDir: &corev1.EmptyDirVolumeSource{
+	//			Medium:    corev1.StorageMediumDefault,
+	//			SizeLimit: &swVolumeQuantity,
+	//		},
+	//	},
+	//}
+	//if len(pod.Spec.Volumes) == 0 {
+	//	patches = append(patches, Patch{OP: OP_ADD, Path: "/spec/volumes", Value: [0]struct{}{}})
+	//}
+	//patches = append(patches, Patch{OP: OP_ADD, Path: "/spec/volumes/", Value: swVolume})
+	//
+	//// addInitContainer
+	//initContainer := corev1.Container{
+	//	Name:  "skywalking",
+	//	Image: config.SWImage,
+	//	VolumeMounts: []corev1.VolumeMount{
+	//		{
+	//			Name:      "skywalking",
+	//			MountPath: "/opt/skywalking",
+	//		},
+	//	},
+	//}
+	//if len(pod.Spec.InitContainers) == 0 {
+	//	patches = append(patches, Patch{OP: OP_ADD, Path: "/spec/initContainers", Value: [0]struct{}{}})
+	//}
+	//patches = append(patches, Patch{OP: OP_ADD, Path: "/spec/initContainers/", Value: initContainer})
+	//
+	//// container cycle
+	//for ic, container := range pod.Spec.Containers {
+	//	if containerMatching(container) {
+	//		mountPath := fmt.Sprintf("/spec/containers/%d/volumeMounts", ic)
+	//
+	//		// volumeMount
+	//		mount := corev1.VolumeMount{
+	//			Name:      "skywalking",
+	//			MountPath: "/opt/skywalking",
+	//		}
+	//		if len(container.VolumeMounts) == 0 {
+	//			patches = append(patches, Patch{OP: OP_ADD, Path: mountPath, Value: [0]struct{}{}})
+	//		}
+	//		patches = append(patches, Patch{OP: OP_ADD, Path: mountPath + "/", Value: mount})
+	//
+	//		// add Init Command and Pods Env
+	//		envPath := fmt.Sprintf("/spec/containers/%d/env", ic)
+	//		envSWArg := "-javaagent:/opt/skywalking/skywalking-agent.jar"
+	//		envOP := OP_ADD
+	//		if len(container.Env) != 0 {
+	//			for ie, env := range container.Env {
+	//				if env.Name == "JAVA_TOOL_OPTIONS" {
+	//					if len(env.Value) != 0 {
+	//						envSWArg = env.Value + " " + envSWArg
+	//						envPath = envPath + "/" + string(ie)
+	//						envOP = OP_REPLACE
+	//					}
+	//				}
+	//			}
+	//		}
+	//		if len(container.Env) == 0 {
+	//			patches = append(patches, Patch{OP: OP_ADD, Path: envPath, Value: [0]struct{}{}})
+	//		}
+	//		if envOP == OP_REPLACE {
+	//			patches = append(patches, Patch{OP: envOP, Path: envPath, Value: envSWArg})
+	//		} else {
+	//			patches = append(patches, Patch{OP: envOP, Path: envPath + "/",
+	//				Value: corev1.EnvVar{Name: "JAVA_TOOL_OPTIONS", Value: envSWArg}})
+	//		}
+	//	}
+	//}
+	//if klog.V(2) {
+	//	res, _ := json.Marshal(patches)
+	//	klog.V(2).Info("Patchs: ", string(res))
+	//}
 	return patches
 }
